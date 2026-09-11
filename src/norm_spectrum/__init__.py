4059e0d793a8076a8c7a01abc13e397f0ebef004
Frank Tramper <ftramper@gmail.com>
2026-09-11 13:01:55 +0200

    Set package version to 1.0.0

--- src/norm_spectrum/__init__.py
+++ src/norm_spectrum/__init__.py
@@ -3,4 +3,4 @@
 from .pseudo import DEFAULT_CONTINUUM_WINDOW, estimate_pseudocontinuum, pseudo_norm
 
 __all__ = ["DEFAULT_CONTINUUM_WINDOW", "estimate_pseudocontinuum", "pseudo_norm"]
-__version__ = "0.1.0"
+__version__ = "1.0.0"
